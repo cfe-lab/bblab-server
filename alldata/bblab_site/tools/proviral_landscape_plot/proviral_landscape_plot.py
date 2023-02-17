cc19f783a4a64a77af4ed37c551e6343dcdf9bca
CBeelen <cha.beelen@gmail.com>
2023-02-16 17:42:02 -0800

    Improve plot

--- alldata/bblab_site/tools/proviral_landscape_plot/proviral_landscape_plot.py
+++ alldata/bblab_site/tools/proviral_landscape_plot/proviral_landscape_plot.py
@@ -1,6 +1,8 @@
 from csv import DictReader
 from argparse import ArgumentParser
 from genetracks import Figure, Track, Multitrack
+from itertools import groupby
+from operator import itemgetter
 
 DEFECT_TO_COLOR = {'5DEFECT': "#440154",
                    'Hypermut': "#1fa187",
@@ -10,8 +12,25 @@ DEFECT_TO_COLOR = {'5DEFECT': "#440154",
                    'LargeDeletion': "#365c8d"}
 HIGHLIGHT_COLORS = {'InternalInversion': "#AFAFAF",
                     'LargeDeletion': "#AFAFAF"}
+DEFECT_ORDER = {'Intact': 10,
+                'Inferred_Intact': 20,
+                'Hypermut': 30,
+                '5DEFECT': 40,
+                'LargeDeletion': 50,
+                'InternalInversion': 60}
 START_POS = 638
 END_POS = 9632
+
+
+def defect_order(defect):
+    try:
+        order = DEFECT_ORDER[defect]
+    except KeyError:
+        max_order = max(DEFECT_ORDER.values())
+        order = max_order + 1
+        DEFECT_ORDER[defect] = order
+        print(f"The order of defect {defect} was not specified - it will just get appended to the end of the plot.")
+    return order
 
 
 def make_gene_track(xstart, xend, defect_type, is_highlighted):
@@ -53,20 +72,31 @@ class ProviralLandscapePlot:
 
     def draw_current_multitrack(self):
         # draw line and reset multitrack
-        self.figure.add(Multitrack(self.curr_multitrack))
+        self.figure.add(Multitrack(self.curr_multitrack), gap=0)
         self.curr_multitrack = []
 
 
 def create_proviral_plot(input_file, output_svg):
-    reader = DictReader(input_file)
     figure = Figure()
     plot = ProviralLandscapePlot(figure)
-    for row in reader:
-        plot.add_line(row['samp_name'],
-                      int(row['ref_start']),
-                      int(row['ref_end']),
-                      row['defect'],
-                      row['highlighted'])
+    lines = list(DictReader(input_file))
+    lines.sort(key=lambda elem: defect_order(elem['defect']))
+    for _, defect_rows in groupby(lines, itemgetter('defect')):
+        defect_rows = list(defect_rows)
+        defect_rows.sort(key=lambda elem: elem['samp_name'])
+        all_rows_by_sample = []
+        for _, samp_rows in groupby(defect_rows, itemgetter('samp_name')):
+            samp_rows = list(samp_rows)
+            samp_rows.sort(key=lambda elem: int(elem['ref_start']))
+            all_rows_by_sample.append(samp_rows)
+        all_rows_by_sample.sort(key=lambda samp_list: -int(samp_list[0]['ref_end']))
+        all_rows_this_defect = [item for sublist in all_rows_by_sample for item in sublist]
+        for row in all_rows_this_defect:
+            plot.add_line(row['samp_name'],
+                          int(row['ref_start']),
+                          int(row['ref_end']),
+                          row['defect'],
+                          row['highlighted'])
     # draw the final line in the plot
     plot.draw_current_multitrack()
     figure.show(w=900).saveSvg(output_svg)
