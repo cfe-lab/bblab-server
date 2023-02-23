40f711db3f6786d80a41cfe5e2c9ac3f8010e5a1
CBeelen <cha.beelen@gmail.com>
2023-02-22 17:11:46 -0800

    WIP: read in data and email 2

---
+++ alldata/bblab_site/tools/proviral_landscape_plot/run_proviral_landscape.py
@@ -0,0 +1,29 @@
+import sys
+import os
+sys.path.append(os.environ.get('BBLAB_UTIL_PATH', 'fail'))
+import web_output
+import mailer
+
+
+def run(csv_data, analysis_id, email_address_string):
+    # Create an instance of the site class for website creation.
+    website = web_output.Site("Proviral Landscape Plot - Results", web_output.SITE_BOXED)
+    website.set_footer('go back to <a href="/django/wiki/" >wiki</a>')
+
+    # Get website input.
+
+    # Send email
+    # Create subject line
+    subject_line = "Proviral Landscape Plot Results"
+    if analysis_id:
+        subject_line += " - {}".format(analysis_id)
+
+    # Add the body to the message and send it.
+    msg_body = (f"Test email for ID {analysis_id}")
+
+    if mailer.send_sfu_email("proviral_landscape_plot", email_address_string, subject_line, msg_body) == 0:
+        website.send(("An email has been sent to <b>{}</b> with a full table of results."
+                      "<br>Make sure <b>{}</b> is spelled correctly.").format(email_address_string,
+                                                                              email_address_string))
+
+    return website.generate_site()
