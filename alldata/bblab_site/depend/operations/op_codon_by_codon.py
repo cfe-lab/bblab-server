c8b92df1de0890a6616b4c0e2663b00296401ffd
Vitaliy Mysak <vmysak@bccfe.ca>
2025-07-02 21:34:00 +0000

    Fix codon-by-codon bugs

--- alldata/bblab_site/depend/operations/op_codon_by_codon.py
+++ alldata/bblab_site/depend/operations/op_codon_by_codon.py
@@ -1,178 +1,311 @@
 # This module is compatible with Python 3.7 #
 import sys, os
+import math
 
 from scipy import stats  # For the p value calculation function.
 
-sys.path.append( os.environ.get('BBLAB_UTIL_PATH', 'fail') ) 
+sys.path.append(os.environ.get("BBLAB_UTIL_PATH", "fail"))
 import sequence_utils
 import math_utils
 
-sys.path.append( os.environ.get('BBLAB_OP_PATH', 'fail') )
+sys.path.append(os.environ.get("BBLAB_OP_PATH", "fail"))
 import op_qvalue
 
-def get_output_matrix( protein_sequences, min_count):
+
+def get_output_matrix(protein_sequences, min_count):
+    """
+    This function processes the inputted protein sequence data and outputs a 'matrix'
+    containing all the data needed to construct the resulting excel file.
+
+    depends on: qvalue, sequence_utils, math_utils, scipy, numpy.
+    """
+
+    # DEBUG: Print function entry and input parameters
+    """
+	print("[DEBUG] get_output_matrix called with:")
+	print(f"  - Number of protein sequences: {len(protein_sequences) if protein_sequences else 0}")
+	print(f"  - min_count: {min_count}")
+	if protein_sequences:
+		print(f"  - First sequence length: {len(protein_sequences[0][1]) if len(protein_sequences[0]) > 1 else 'N/A'}")
+		print(f"  - First few sequences: {protein_sequences[:3] if len(protein_sequences) > 0 else 'None'}")
+	print(f"[DEBUG] Timestamp: {__import__('datetime').datetime.now()}")
+	print("[DEBUG] This is the UPDATED version with fixes (2024-12-27)")
 	"""
-	This function processes the inputted protein sequence data and outputs a 'matrix'
-	containing all the data needed to construct the resulting excel file.
 
-	depends on: qvalue, sequence_utils, math_utils, scipy, numpy. 
-	"""
+    ##### Class and function definitions
 
-	##### Class and function definitions
+    class ProteinData:
+        """
+        This class holds the data for a single protein character in its column column.
+        """
 
-	class ProteinData:
-		'''
-		This class holds the data for a single protein character in its column column.
-		'''
+        def __init__(self, char, decimal_list):
+            self.protein_char = char
+            self.decimal_list = decimal_list
 
-		def __init__(self, char, decimal_list):
-			self.protein_char = char
-			self.decimal_list = decimal_list
+        # This function returns how many times this protein characters appears by getting the data
+        # from the decimal value list's length.
+        def get_occurrances(self):
+            return len(self.decimal_list)
 
-		# This function returns how many times this protein characters appears by getting the data
-		# from the decimal value list's length.
-		def get_occurrances(self):
-			return len(self.decimal_list)
+    class ColumnData:
+        """
+        This class holds the ProteinData for everything in this column (of the input data).
+        """
 
-	class ColumnData:
-		'''
-		This class holds the ProteinData for everything in this column (of the input data).
-		'''
-		
-		# nw_protein_list is a list of ProteinData classes, while with_protein_data is just one.
-		def __init__(self, column_pos, w_protein_dat, nw_protein_list):
-			self.with_protein_data = w_protein_dat
-			self.not_with_protein_data_list = nw_protein_list
-			self.position = column_pos
+        # nw_protein_list is a list of ProteinData classes, while with_protein_data is just one.
+        def __init__(self, column_pos, w_protein_dat, nw_protein_list):
+            self.with_protein_data = w_protein_dat
+            self.not_with_protein_data_list = nw_protein_list
+            self.position = column_pos
 
-		def get_with(self):
-			return self.with_protein_data
+        def get_with(self):
+            return self.with_protein_data
 
-		def get_not_with_list(self):
-			return self.not_with_protein_data_list
-	
-		# This function gets all the decimal lists from not-with and combines them.
-		def get_not_with_decimal_list(self):
-			sum_decimal_list = []
-			for data in self.not_with_protein_data_list:
-				sum_decimal_list += data.decimal_list
-			return sum_decimal_list
+        def get_not_with_list(self):
+            return self.not_with_protein_data_list
 
-		# This function gets all the occurrance values from not-with and combines them into one number.
-		def get_not_with_occurrances(self):
-			sum_occurrance_count = 0
-			for data in self.not_with_protein_data_list:
-				sum_occurrance_count += data.get_occurrances()
-			return sum_occurrance_count
-	
-	class ColumnOutput:
-		'''
-		ColumnOutput contains the information that will be formatted into a .xlsx file.
-		'''
-	
-		def __init__(self, coord, with_amino, with_median, notwith_median, with_count, notwith_count, p_value, q_value):
-			self.coord = coord
-			self.w_amino = with_amino
-			self.w_median = with_median
-			self.nw_median = notwith_median
-			self.w_count = with_count
-			self.nw_count = notwith_count
-			self.p_value = p_value
-			self.q_value = q_value
-	
-		# This function formats all the information and returns it as a list. ( row )
-		def get_formatted_row(self):
-			return [ self.coord, self.w_amino, self.w_median, self.nw_median, self.w_count, self.nw_count, self.p_value, self.q_value ]
+        # This function gets all the decimal lists from not-with and combines them.
+        def get_not_with_decimal_list(self):
+            sum_decimal_list = []
+            for data in self.not_with_protein_data_list:
+                sum_decimal_list += data.decimal_list
+            return sum_decimal_list
 
+        # This function gets all the occurrance values from not-with and combines them into one number.
+        def get_not_with_occurrances(self):
+            sum_occurrance_count = 0
+            for data in self.not_with_protein_data_list:
+                sum_occurrance_count += data.get_occurrances()
+            return sum_occurrance_count
 
-	# This function extracts and calculates the needed data from the ColumnData class.
-	def _run_seq_test( column_data ):
-		w_amino = column_data.get_with().protein_char  # Get Protein char for with.
-	
-		w_decimal_list = column_data.get_with().decimal_list  # Save the decimal list for later.
-		w_median = math_utils.median( w_decimal_list )
-	
-		# Find the median of all not-with decimal values.
-		nw_decimal_list = column_data.get_not_with_decimal_list()
-		nw_median = math_utils.median( nw_decimal_list )
-	
-		w_count = column_data.get_with().get_occurrances()
-	
-		# Find the sum of all not-with occurance counts.
-		nw_count = column_data.get_not_with_occurrances()
-	
-		p_value = math_utils.round_sf(stats.kruskal(w_decimal_list, nw_decimal_list)[1], 5)  # Use scipy to find the p-values.	
-		
-		# Put all this information into the ColumnOutput class.
-		output_column = ColumnOutput( column_data.position+1, w_amino, w_median, nw_median, w_count, nw_count, p_value, -1)
-	
-		# Send the output column to the output matrix.
-		output_matrix.append( output_column ) 
+    class ColumnOutput:
+        """
+        ColumnOutput contains the information that will be formatted into a .xlsx file.
+        """
 
-	##### Run tests on the given sequences. 
+        def __init__(
+            self,
+            coord,
+            with_amino,
+            with_median,
+            notwith_median,
+            with_count,
+            notwith_count,
+            p_value,
+            q_value,
+        ):
+            self.coord = coord
+            self.w_amino = with_amino
+            self.w_median = with_median
+            self.nw_median = notwith_median
+            self.w_count = with_count
+            self.nw_count = notwith_count
+            self.p_value = p_value
+            self.q_value = q_value
 
-	sequence_length = len(protein_sequences[0][1])  # Init the length to be the length of the first protein sequence.
-	output_matrix = []  # This matrix holds the data that will be added to the excel file.(and returned) This is a list of dictionaries, so... matrix?
+        # This function formats all the information and returns it as a list. ( row )
+        def get_formatted_row(self):
+            return [
+                self.coord,
+                self.w_amino,
+                self.w_median,
+                self.nw_median,
+                self.w_count,
+                self.nw_count,
+                self.p_value,
+                self.q_value,
+            ]
 
-	# Iterate over each item in the sequences.
-	for column_index in range(sequence_length):
-		# Create and fill a list that contains all proteins at the current position and their decimal value.
-		codon_list = [] # -> [ (char, decimal), ... ]
-		for seq_index in range(len(protein_sequences)):
-			codon_list.append( (protein_sequences[seq_index][1][column_index], protein_sequences[seq_index][0]) )  # list of -> (character, decimal)
-	
-		# Count how many of each protein there is.
-		data_dict = {}  # This is a dict of ProteinData classes with char as the key. -> { char : ProteinData }
-		valid_characters = 0  # This holds how many valid characters were counted.  ( For checking validity. )
-		for tup in codon_list:
-			char = tup[0]
-			decimal = float(tup[1])
+    # This function extracts and calculates the needed data from the ColumnData class.
+    def _run_seq_test(column_data):
+        w_amino = column_data.get_with().protein_char  # Get Protein char for with.
 
-			if not char in sequence_utils.protein_mixture_list:  # Make sure that invalid characters (mixtures) are not counted in this step.
-				valid_characters += 1
+        w_decimal_list = (
+            column_data.get_with().decimal_list
+        )  # Save the decimal list for later.
+        w_median = math_utils.median(w_decimal_list)
 
-				if char in data_dict:
-					data_dict[ char ].decimal_list += [ decimal ]  # Populate the decimal list.
-				else:
-					data_dict[ char ] = ProteinData( char, [decimal] )  # Init the ProteinData class.
-	
-		# Find the proteins that have an occurance value higher than minCount.
-		if len(data_dict) > 1:  # Case: there are at least two different proteins in this column.
-			valid_data = [] 
+        # Find the median of all not-with decimal values.
+        nw_decimal_list = column_data.get_not_with_decimal_list()
+        nw_median = math_utils.median(nw_decimal_list)
 
-			for data in data_dict.values():	
-				# Check if current char is a vaild group.
-				if data.get_occurrances() >= min_count and valid_characters - data.get_occurrances() >= min_count:
-					valid_data += [ data ]
-		else:
-			continue  # Case: all characters equal, ignore.
+        w_count = column_data.get_with().get_occurrances()
+        # Find the sum of all not-with occurance counts.
+        nw_count = column_data.get_not_with_occurrances()
 
-		if len(valid_data) <= 0:
-			continue  # Case: no valid data groups.
-	
-	
-		# Do analysis
-		for index in range( 0, len(valid_data) ):
-			# Put with and not with into the same container.
-			data_dict.pop( valid_data[index].protein_char )  # Temp pop.
+        # Calculate p-value with proper validation
+        try:
+            # Check if we have sufficient data for statistical test
+            if (
+                len(w_decimal_list) < 2
+                or len(nw_decimal_list) < 2
+                or len(w_decimal_list) == 0
+                or len(nw_decimal_list) == 0
+            ):
+                # Not enough data for meaningful statistical test
+                p_value = 1.0  # Non-significant p-value
+            else:
+                # Check if all values are identical (no variance)
+                w_variance = len(set(w_decimal_list)) > 1
+                nw_variance = len(set(nw_decimal_list)) > 1
 
-			column_data = ColumnData( column_index, valid_data[index], data_dict.values() )
-			_run_seq_test( column_data )  # Run test on the data.
-	
-			data_dict[valid_data[index].protein_char] = valid_data[index]  # Add the item back to the dictionary. 
-	
-	# Generate qvalues using the entire list of pvalues.	
-	pvalue_list = [] 
-	for item in output_matrix:
-		pvalue_list.append( item.get_formatted_row()[6] ) 
+                if not w_variance and not nw_variance:
+                    # No variance in either group - no meaningful difference
+                    p_value = 1.0
+                else:
+                    # Perform Kruskal-Wallis test
+                    kruskal_result = stats.kruskal(w_decimal_list, nw_decimal_list)
+                    raw_p_value = kruskal_result[1]
 
-	qvalue_list = op_qvalue.get_qvalues( pvalue_list )  # Get q values using the r script.
-	
-	# Validate that we have the same number of q-values as p-values
-	if len(qvalue_list) != len(output_matrix):
-		raise RuntimeError(f"Mismatch between q-values ({len(qvalue_list)}) and output matrix ({len(output_matrix)})")
-	
-	for matrix_index in range(0, len(output_matrix)):
-		output_matrix[matrix_index].q_value = qvalue_list[matrix_index]
+                    # Validate the result
+                    if (
+                        math.isnan(raw_p_value)
+                        or math.isinf(raw_p_value)
+                        or raw_p_value < 0
+                        or raw_p_value > 1
+                    ):
+                        # Invalid p-value from statistical test
+                        p_value = 1.0  # Conservative non-significant value
+                    else:
+                        p_value = math_utils.round_sf(raw_p_value, 5)
+        except Exception:
+            # Any error in statistical calculation - use conservative p-value
+            p_value = 1.0
 
-	return output_matrix
+        # Put all this information into the ColumnOutput class.
+        output_column = ColumnOutput(
+            column_data.position + 1,
+            w_amino,
+            w_median,
+            nw_median,
+            w_count,
+            nw_count,
+            p_value,
+            -1,
+        )
+
+        # Send the output column to the output matrix.
+        output_matrix.append(output_column)
+
+    ##### Run tests on the given sequences.
+
+    sequence_length = len(
+        protein_sequences[0][1]
+    )  # Init the length to be the length of the first protein sequence.
+    output_matrix = []  # This matrix holds the data that will be added to the excel file.(and returned) This is a list of dictionaries, so... matrix?
+
+    # Iterate over each item in the sequences.
+    for column_index in range(sequence_length):
+        # Create and fill a list that contains all proteins at the current position and their decimal value.
+        codon_list = []  # -> [ (char, decimal), ... ]
+        for seq_index in range(len(protein_sequences)):
+            codon_list.append(
+                (
+                    protein_sequences[seq_index][1][column_index],
+                    protein_sequences[seq_index][0],
+                )
+            )  # list of -> (character, decimal)
+
+        # Count how many of each protein there is.
+        data_dict = {}  # This is a dict of ProteinData classes with char as the key. -> { char : ProteinData }
+        valid_characters = 0  # This holds how many valid characters were counted.  ( For checking validity. )
+        for tup in codon_list:
+            char = tup[0]
+            decimal = float(tup[1])
+
+            if (
+                char not in sequence_utils.protein_mixture_list
+            ):  # Make sure that invalid characters (mixtures) are not counted in this step.
+                valid_characters += 1
+
+                if char in data_dict:
+                    data_dict[char].decimal_list += [
+                        decimal
+                    ]  # Populate the decimal list.
+                else:
+                    data_dict[char] = ProteinData(
+                        char, [decimal]
+                    )  # Init the ProteinData class.
+
+        # Find the proteins that have an occurance value higher than minCount.
+        if (
+            len(data_dict) > 1
+        ):  # Case: there are at least two different proteins in this column.
+            valid_data = []
+
+            print(
+                f"[DEBUG] Pos {column_index + 1}: Found {len(data_dict)} unique proteins. Total valid chars: {valid_characters}. min_count: {min_count}"
+            )
+            for char, data in data_dict.items():
+                with_count = data.get_occurrances()
+                not_with_count = valid_characters - with_count
+                print(
+                    f"[DEBUG]   - Char '{char}': count={with_count}, other_count={not_with_count}. Meets criteria? {with_count >= min_count and not_with_count >= min_count}"
+                )
+
+            for data in data_dict.values():
+                # Check if current char is a vaild group.
+                if (
+                    data.get_occurrances() >= min_count
+                    and valid_characters - data.get_occurrances() >= min_count
+                ):
+                    valid_data += [data]
+        else:
+            print(
+                f"[DEBUG] Pos {column_index + 1}: Skipping, only one unique protein found."
+            )
+            continue  # Case: all characters equal, ignore.
+
+        if len(valid_data) <= 0:
+            print(
+                f"[DEBUG] Pos {column_index + 1}: No valid data groups found meeting min_count of {min_count}.\n"
+            )
+            continue  # Case: no valid data groups.
+
+        # Do analysis
+        for index in range(0, len(valid_data)):
+            # Put with and not with into the same container.
+            data_dict.pop(valid_data[index].protein_char)  # Temp pop.
+
+            column_data = ColumnData(
+                column_index, valid_data[index], data_dict.values()
+            )
+            _run_seq_test(column_data)  # Run test on the data.
+
+            data_dict[valid_data[index].protein_char] = valid_data[
+                index
+            ]  # Add the item back to the dictionary.
+
+    # Generate qvalues using the entire list of pvalues.
+
+    # Handle empty output matrix
+    if not output_matrix:
+        return output_matrix
+
+    # Extract p-values (now guaranteed to be valid from the statistical calculation)
+    pvalue_list = []
+    for i, item in enumerate(output_matrix):
+        raw_pvalue = item.get_formatted_row()[6]
+        pvalue_list.append(raw_pvalue)
+        """
+		print(f"[DEBUG] Item {i}: p-value = {raw_pvalue}, type = {type(raw_pvalue)}")
+		"""
+
+    # All p-values should now be valid, but add a final safety check
+    if not pvalue_list:
+        return output_matrix
+
+    # Call qvalue function
+    qvalue_list = op_qvalue.get_qvalues(pvalue_list)  # Get q values using the r script.
+
+    # Validate that we have the same number of q-values as p-values
+    if len(qvalue_list) != len(output_matrix):
+        raise RuntimeError(
+            f"Mismatch between q-values ({len(qvalue_list)}) and output matrix ({len(output_matrix)})"
+        )
+
+    for matrix_index in range(0, len(output_matrix)):
+        output_matrix[matrix_index].q_value = qvalue_list[matrix_index]
+
+    return output_matrix
--- alldata/bblab_site/depend/operations/op_qvalue.py
+++ alldata/bblab_site/depend/operations/op_qvalue.py
@@ -4,54 +4,164 @@ import subprocess
 import tempfile
 import os
 
-def get_qvalues( pvalues ):
-	''' 
-	This function uses the r script to convert a list of pvalues into
-	a list of qvalues.
 
-	Depends On: R
-	'''
+def get_qvalues(pvalues):
+    """
+    This function uses the r script to convert a list of pvalues into
+    a list of qvalues.
 
-	# This block runs the R script from the using the commandline and converts the console output into a list.
-	qvalues = []
-	with tempfile.TemporaryFile() as tmpf:
-		proc = subprocess.Popen(['Rscript', '{}qvalue_calculate.r'.format(os.environ.get('BBLAB_R_PATH', 'fail')), str(pvalues)],
-					 stdout=tmpf, stderr=subprocess.PIPE) 
-		proc.wait()
-		
-		# Check for R script errors
-		if proc.returncode != 0:
-			_, stderr = proc.communicate()
-			raise RuntimeError(f"R script failed with return code {proc.returncode}: {stderr.decode('utf-8') if stderr else 'Unknown error'}")
-		
-		tmpf.seek(0)
-		raw_output = tmpf.read().decode("utf-8").strip()
-		
-		# Handle empty output
-		if not raw_output:
-			raise RuntimeError("R script produced no output")
-		
-		# Robust parsing: handle both comma-separated and space-separated formats
-		if ',' in raw_output:
-			# Comma-separated format (new R versions)
-			qvalue_strings = raw_output.split(',')
-		else:
-			# Space-separated format (old R versions)
-			qvalue_strings = raw_output.split()
-		
-		# Convert to floats, filtering empty strings
-		qvalues = []
-		for qstr in qvalue_strings:
-			qstr = qstr.strip()
-			if qstr:
-				try:
-					qvalues.append(float(qstr))
-				except ValueError:
-					# Skip invalid values but log the issue
-					continue
-		
-		# Validate that we got the expected number of q-values
-		if len(qvalues) != len(pvalues):
-			raise RuntimeError(f"R script returned {len(qvalues)} q-values but expected {len(pvalues)} (input p-values: {len(pvalues)})")
-	
-	return qvalues
+    Depends On: R
+    """
+    # v2025-07-02c: Maximum debug output for R script interaction
+    # print(f"[DEBUG] get_qvalues called with {len(pvalues)} p-values")
+    # print(f"[DEBUG] Raw p-values: {pvalues}")
+
+    # Input validation and cleaning
+    if not pvalues:
+        # print("[DEBUG] No p-values provided, returning empty list")
+        return []
+
+    # Clean and validate p-values - be extremely defensive
+    cleaned_pvalues = []
+    for i, pval in enumerate(pvalues):
+        try:
+            # Convert to float and validate
+            if pval is None:
+                # print(f"[DEBUG] P-value at index {i} is None, using fallback 1.0")
+                pval_float = 1.0  # Conservative fallback
+            elif isinstance(pval, str):
+                try:
+                    pval_float = float(pval)
+                    # print(f"[DEBUG] P-value at index {i}: '{pval}' -> {pval_float}")
+                except (ValueError, TypeError):
+                    # print(f"[DEBUG] P-value at index {i}: '{pval}' could not be converted, using fallback 1.0")
+                    pval_float = 1.0  # Conservative fallback
+            else:
+                pval_float = float(pval)
+                # print(f"[DEBUG] P-value at index {i}: {pval} -> {pval_float}")
+
+            # Check for invalid values
+            import math
+
+            if (
+                math.isnan(pval_float)
+                or math.isinf(pval_float)
+                or pval_float < 0
+                or pval_float > 1
+            ):
+                # print(f"[DEBUG] P-value at index {i}: {pval_float} is invalid, using fallback 1.0")
+                pval_float = 1.0  # Conservative fallback
+
+            cleaned_pvalues.append(pval_float)
+
+        except Exception as e:
+            # Any error - use conservative fallback
+            # print(f"[DEBUG] P-value at index {i}: Exception {e}, using fallback 1.0")
+            cleaned_pvalues.append(1.0)
+
+    # print(f"[DEBUG] After cleaning: {len(cleaned_pvalues)} valid p-values: {cleaned_pvalues}")
+
+    # Final validation - ensure we have valid p-values
+    for pval in cleaned_pvalues:
+        if not isinstance(pval, (int, float)) or pval < 0 or pval > 1:
+            raise ValueError(
+                f"Invalid p-value after cleaning: {pval}. All p-values must be numeric and in range [0,1]"
+            )
+
+    # This block runs the R script from the using the commandline and converts the console output into a list.
+    qvalues = []
+    r_script_path = "{}qvalue_calculate.r".format(
+        os.environ.get("BBLAB_R_PATH", "fail")
+    )
+    r_input = str(cleaned_pvalues)
+
+    # print(f"[DEBUG] R script path: {r_script_path}")
+    # print(f"[DEBUG] R script input: {r_input}")
+    # print(f"[DEBUG] R script input type: {type(r_input)}")
+    # print(f"[DEBUG] R script input length: {len(r_input)}")
+
+    with tempfile.TemporaryFile() as tmpf:
+        proc = subprocess.Popen(
+            ["Rscript", r_script_path, r_input], stdout=tmpf, stderr=subprocess.PIPE
+        )
+        _, stderr = proc.communicate()
+
+        # print(f"[DEBUG] R script return code: {proc.returncode}")
+
+        # Check for R script errors
+        if proc.returncode != 0:
+            stderr_msg = stderr.decode("utf-8") if stderr else "Unknown error"
+            print(f"[ERROR] R script failed with return code {proc.returncode}")
+            print(f"[ERROR] R script stderr: {stderr_msg}")
+            raise RuntimeError(
+                f"R script failed with return code {proc.returncode}. Input p-values: {cleaned_pvalues}. Error: {stderr_msg}"
+            )
+
+        tmpf.seek(0)
+        raw_output = tmpf.read().decode("utf-8").strip()
+
+        # print(f"[DEBUG] R script raw output length: {len(raw_output)}")
+        # print(f"[DEBUG] R script raw output (repr): {repr(raw_output)}")
+        # print(f"[DEBUG] R script raw output (formatted): '{raw_output}'")
+        # print(f"[DEBUG] R script stderr content: '{stderr.decode('utf-8') if stderr else 'None'}'")
+
+        # Handle empty output
+        if not raw_output:
+            print(
+                f"[ERROR] R script produced no output for input p-values: {cleaned_pvalues}"
+            )
+            raise RuntimeError(
+                f"R script produced no output for input p-values: {cleaned_pvalues}"
+            )
+
+        # Robust parsing: handle different R output formats
+        # R script returns space-separated values with a trailing comma: "0.1 0.2 0.3 ,"
+        if raw_output.endswith(","):
+            # Space-separated format with trailing comma
+            # print("[DEBUG] Parsing output as space-separated values with trailing comma")
+            # Remove trailing comma and split by space
+            clean_output = raw_output.rstrip(",").strip()
+            qvalue_strings = clean_output.split()
+        elif "," in raw_output and " " not in raw_output:
+            # Pure comma-separated format (no spaces): "0.1,0.2,0.3"
+            # print("[DEBUG] Parsing output as comma-separated values")
+            qvalue_strings = raw_output.split(",")
+        else:
+            # Space-separated format (old R versions): "0.1 0.2 0.3"
+            # print("[DEBUG] Parsing output as space-separated values")
+            qvalue_strings = raw_output.split()
+
+        # print(f"[DEBUG] Split into {len(qvalue_strings)} parts: {qvalue_strings}")
+
+        # Convert to floats, filtering empty strings
+        qvalues = []
+        for j, qstr in enumerate(qvalue_strings):
+            qstr = qstr.strip()
+            # print(f"[DEBUG] Processing q-value string {j}: '{qstr}'")
+            if qstr:
+                try:
+                    qval = float(qstr)
+                    # print(f"[DEBUG] Converted q-value {j}: '{qstr}' -> {qval}")
+                    qvalues.append(qval)
+                except ValueError as e:
+                    # Skip invalid values but log the issue
+                    print(
+                        f"[ERROR] Could not convert q-value {j}: '{qstr}' (error: {e})"
+                    )
+                    continue
+
+        # print(f"[DEBUG] Final parsed q-values: {qvalues}")
+
+        # Validate that we got the expected number of q-values
+        if len(qvalues) != len(cleaned_pvalues):
+            print(
+                f"[ERROR] R script returned {len(qvalues)} q-values but expected {len(cleaned_pvalues)}"
+            )
+            print(f"[ERROR] Input p-values ({len(cleaned_pvalues)}): {cleaned_pvalues}")
+            print(f"[ERROR] Output q-values ({len(qvalues)}): {qvalues}")
+            raise RuntimeError(
+                f"R script returned {len(qvalues)} q-values but expected {len(cleaned_pvalues)} (input p-values: {len(cleaned_pvalues)})"
+            )
+
+    # print(f"[DEBUG] Returning {len(qvalues)} q-values: {qvalues}")
+    return qvalues
