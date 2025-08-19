ebe5d718c72c406add74913572a9460c3bd656b7
Vitaliy Mysak <vmysak@bccfe.ca>
2025-08-19 00:32:11 +0000

    Prepare plot file for sending

--- alldata/bblab_site/tools/proviral_landscape_plot/run_proviral_landscape.py
+++ alldata/bblab_site/tools/proviral_landscape_plot/run_proviral_landscape.py
@@ -3,6 +3,8 @@ import os
 sys.path.append(os.environ.get('BBLAB_UTIL_PATH', 'fail'))
 import web_output
 import mailer
+import json
+from pathlib import Path
 from . import proviral_landscape_plot
 
 
@@ -17,15 +19,29 @@ def run(csv_data, analysis_id, email_address_string):
 
     website.send("<img width='100%' src='/media/output.svg' alt='Output svg'")
 
+    short_description = analysis_id
+    if len(short_description) > 30:
+        short_description = short_description[:30] + "..."
+
     # Send email
     # Create subject line
     subject_line = "Proviral Landscape Plot Results"
-    if analysis_id:
-        subject_line += " - {}".format(analysis_id)
+    if short_description:
+        subject_line += " - {}".format(short_description)
 
     # Add the body to the message and send it.
-    msg_body = (f"Test email for ID {analysis_id}")
+    quoted_description = json.dumps(short_description)
+    msg_body = f"Your landscape plot, described as {quoted_description}, is ready. See the attachment."
 
+    #
+    # Prepare the file
+    #
+    svg_content = Path(output_svg).read_text()
+    plot_file = mailer.create_file( short_description, 'svg', svg_content )
+
+    #
+    # Actual send
+    #
     if mailer.send_sfu_email("proviral_landscape_plot", email_address_string, subject_line, msg_body) == 0:
         website.send(("An email has been sent to <b>{}</b> with your image."
                       "<br>Make sure <b>{}</b> is spelled correctly.").format(email_address_string,
