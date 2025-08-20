b0579e091d3917d3ae28c22be79275c683d39401
Vitaliy Mysak <vmysak@bccfe.ca>
2025-08-20 20:31:16 +0000

    Improve warning wording

--- alldata/bblab_site/tools/proviral_landscape_plot/run_proviral_landscape.py
+++ alldata/bblab_site/tools/proviral_landscape_plot/run_proviral_landscape.py
@@ -300,7 +300,7 @@ def run(csv_data, analysis_id, email_address_string):
 
     if warnings:
         website.send("<h2 style='color:#8a6d3b'>We noticed some issues</h2>")
-        website.send("<p>These items may not stop the plot from being made, but they might affect the result. Please check them and correct if needed.</p>")
+        website.send("<p>We could create the plot despite them, but these items below might have affected the result. Please check them and correct if needed.</p>")
         website.send(_make_box('Things to check', warnings, '#f0ad4e', '#fcf8e3', '#8a6d3b'))
         website.send("<p>The plot could be made, but please review the warnings above for possible problems.</p>")
 
