bd0ae6871263042b3b3515fcfa87acd100d3d50f
Vitaliy Mysak <vmysak@bccfe.ca>
2026-02-05 22:13:46 +0000

    Rename inputs.py to parser.py

--- alldata/bblab_site/tools/isoforms_plot/isoforms_plot/__main__.py
+++ alldata/bblab_site/tools/isoforms_plot/isoforms_plot/__main__.py
@@ -4,21 +4,19 @@ import sys
 from typing import Sequence
 
 from isoforms_plot import plotter
-from isoforms_plot import inputs
+from isoforms_plot import parser
 
 
 def main(argv: Sequence[str]) -> int:
-    parser = ArgumentParser()
-    parser.add_argument("input_csv", type=Path,
-                        help="Input CSV")
-    parser.add_argument("output_svg", type=Path,
-                        help="Output SVG")
-    args = parser.parse_args(argv)
+    argparser = ArgumentParser()
+    argparser.add_argument("input_csv", type=Path, help="Input CSV")
+    argparser.add_argument("output_svg", type=Path, help="Output SVG")
+    args = argparser.parse_args(argv)
 
-    parsed = inputs.parse(args.input_csv)
+    parsed = parser.parse(args.input_csv)
     title = parsed["title"]
 
-    plotter.create_isoforms_plot(inputs.TRANSCRIPTS, inputs.GROUPS, title, inputs.SPLICING_SITES, args.output_svg)
+    plotter.create_isoforms_plot(parser.TRANSCRIPTS, parser.GROUPS, title, parser.SPLICING_SITES, args.output_svg)
     return 0
 
 
