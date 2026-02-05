b217d084b4b56d4cf99653c594992653f2499ed0
Vitaliy Mysak <vmysak@bccfe.ca>
2026-02-05 19:56:31 +0000

    Improve main handling

--- alldata/bblab_site/tools/isoforms_plot/isoforms_plot/__main__.py
+++ alldata/bblab_site/tools/isoforms_plot/isoforms_plot/__main__.py
@@ -1,16 +1,22 @@
 from argparse import ArgumentParser
+import sys
+from typing import Sequence
 
 from isoforms_plot import plotter
 
 
-def main():
+def main(argv: Sequence[str]) -> int:
     parser = ArgumentParser()
     parser.add_argument("output_svg",
                         help="Output SVG")
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
 
     plotter.create_isoforms_plot(None, args.output_svg)
+    return 0
 
 
-if __name__ == '__main__':
-    main()
+def entry() -> None:
+    sys.exit(main(sys.argv[1:]))
+
+
+if __name__ == '__main__': entry()
