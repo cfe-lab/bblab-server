359850d422da0b538871fee569141630825efc67
Vitaliy Mysak <vmysak@bccfe.ca>
2026-02-05 23:05:51 +0000

    Accept title from CLI

--- alldata/bblab_site/tools/isoforms_plot/isoforms_plot/plotter.py
+++ alldata/bblab_site/tools/isoforms_plot/isoforms_plot/plotter.py
@@ -612,7 +612,7 @@ class TranscriptLine:
         return d
 
 
-def plot(transcripts, groups, title, splicing_sites) -> draw.Drawing:
+def plot(transcripts, groups, splicing_sites, title=None) -> draw.Drawing:
     # Use transcripts to determine number of samples to display
     total_samples = len(transcripts)
     figure = Figure()
