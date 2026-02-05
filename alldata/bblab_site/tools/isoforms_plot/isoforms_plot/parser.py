6b40f7add2e37834829ae6e9059193f48e6a6509
Vitaliy Mysak <vmysak@bccfe.ca>
2026-02-05 23:12:43 +0000

    Refactor transcript parsing structure and add data classes for Fragment and Transcript

--- alldata/bblab_site/tools/isoforms_plot/isoforms_plot/parser.py
+++ alldata/bblab_site/tools/isoforms_plot/isoforms_plot/parser.py
@@ -8,16 +8,34 @@
 #
 
 import csv
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Iterator, Optional, Sequence
+
+
+@dataclass(frozen=True)
+class Fragment:
+    start: int
+    end: Optional[int]
+
+
+@dataclass(frozen=True)
+class Transcript:
+    fragments: Sequence[Fragment]
+    label: Optional[str]
+    group: Optional[str]
+    comment: Optional[str]
+
+
+def read_transcripts(reader: csv.DictReader) -> Iterator[Transcript]:
+    raise NotImplementedError("Parsing from CSV is not implemented yet.")
 
 
 def parse(input_file: Path) -> dict:
     with input_file.open() as fd:
         reader = csv.DictReader(fd)
-        assert reader.fieldnames is not None
-        transcripts: list = []
+        transcripts = tuple(read_transcripts(reader))
 
     return {
         'transcripts': transcripts,
     }
-
